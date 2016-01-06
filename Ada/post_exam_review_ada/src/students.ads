e03a452f1050f1e86772edb41b554922924ef004
frmfl <florian.froschermeier@gmail.com>
2016-01-06 12:28:26 +0100

    Instances of student tasks added. Ravenscar forbids task hierarchies -> system is master

--- Ada/post_exam_review_ada/src/students.ads
+++ Ada/post_exam_review_ada/src/students.ads
@@ -1,8 +1,12 @@
 with Review_Settings;
+use Review_Settings;
 
 package students is
 
-    -- static task at package level. TODO: make three of them.
-    task task_stud;
+   -- static task at package level. TODO: make three of them.
+   task type task_stud is
+      --entry Print ( whatever : in integer );   //ravenscar forbids entries
+   end task_stud;
+
 
 end students;
